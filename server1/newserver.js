e833dce6cbfd5d0cceea0631389994959a9bfed9
Samnoon Abrar <samnoonabrar@gmail.com>
2020-11-19 10:14:35 +0600

    done

--- server1/newserver.js
+++ server1/newserver.js
@@ -1,9 +1,20 @@
-var http = require('http');
 
+var fs = require('fs');
+var http = require('http');
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Contenty-type': 'text/plain'});
-    res.write('Hello world');
-    res.end();
+    if(req.url=='/'){
+        let data = fs.readFileSync('home.html', 'utf8');
+        res.end(data);
+    }
+    else if(req.url=='/contact'){
+        let data = fs.readFileSync('contact.html', 'utf8');
+        res.end(data);
+    }
+    else if(req.url=='/about'){
+        let data = fs.readFileSync('about.html', 'utf8');
+        res.end(data);
+    }
 
 });
-server.listen(1000);
+server.listen(2000);
+console.log("DONE");
